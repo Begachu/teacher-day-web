d9d4778ef19a2ce5525e18443f71dd6e36b7fc9d
서예지 <syg9272@naver.com>
2023-05-15 15:44:44 +0900

    🐛 fix simple error

--- src/pages/Letter.tsx
+++ src/pages/Letter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Memo from "../components/Memo";
 import LetterData from "../constants/letter.json";
 const letters = LetterData.data;
@@ -8,10 +7,7 @@ function Letter() {
     <div className="w-full flex flex-col items-center">
       {/* HEADER */}
       <header className="w-full relative h-96 flex bg-background/50 justify-between items-end p-14 overflow-hidden">
-        <img
-          src="단체사진.jpg"
-          className="blur-sm absolute -z-10 w-full scale-125 -top-80"
-        />
+        <img src="단체사진.jpg" className="blur-sm absolute -z-10 w-full scale-125 -top-80" />
         <div>
           <h1 className="text-white flex flex-col text-6xl ml-4">
             <span>손병찬 교수님</span>
@@ -29,19 +25,14 @@ function Letter() {
       <main className="flex flex-wrap gap-5 justify-center items-baseline w-full max-w-6xl py-10">
         <section className="flex-1 min-w-[300px] flex flex-col gap-5 justify-center max-w-2xl">
           {letters
-            .filter((item, index) => index % 3 === 0)
+            .filter((_item, index) => index % 3 === 0)
             .map(({ content, name }, index) => (
-              <Memo
-                key={index}
-                content={content}
-                name={name}
-                color={index % 4}
-              />
+              <Memo key={index} content={content} name={name} color={index % 4} />
             ))}
         </section>
         <section className="flex-1 min-w-[300px] flex flex-col gap-5 justify-center max-w-3xl">
           {letters
-            .filter((item, index) => index % 3 === 1)
+            .filter((_item, index) => index % 3 === 1)
             .map(({ content, name }, index) => (
               <Memo
                 key={index}
@@ -53,14 +44,9 @@ function Letter() {
         </section>
         <section className="flex-1 min-w-[300px] flex flex-col gap-5 justify-center max-w-3xl">
           {letters
-            .filter((item, index) => index % 3 === 2)
+            .filter((_item, index) => index % 3 === 2)
             .map(({ content, name }, index) => (
-              <Memo
-                key={index}
-                content={content}
-                name={name}
-                color={(index + 2) % 4}
-              />
+              <Memo key={index} content={content} name={name} color={(index + 2) % 4} />
             ))}
         </section>
       </main>
