e7c08104c9bf4a16591c12fbd0257a03170090bc
한유경 <ygo65312@naver.com>
2023-05-15 15:41:00 +0900

    update letter page

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -12,19 +12,28 @@ function Memo({ content, name, color }: Props) {
   const letter = content.split("\n");
 
   return (
-    <div className={`${COLOR_SET[color]} p-6 rounded-md shadow-md flex-1`}>
-      <h2 className="text-3xl">{letter[0]}</h2>
+    <div
+      className={`${COLOR_SET[color]} p-6 rounded-md shadow-md flex-1
+    hover:scale-110 hover:shadow-xl hover:z-10 relative transition-all`}
+    >
+      <h2 className="text-3xl mb-2">{letter[0]}</h2>
       {letter.map((item, index) => {
         if (index !== 0)
           return (
-            <span key={`letter-line-${index}`} className="text-2xl font-thin">
+            <span key={`letter-line-${index}`} className="text-2xl font-light">
               {item}
             </span>
           );
       })}
-      <div>
-        <span className="text-xl font-thin"> - {name}</span>
-        <img src={`assets/face/${name}.png`} alt={`${name} 사진`} />
+      <div className="flex items-center justify-end gap-2">
+        <span className="text-xl font-thin mt-2">{name}</span>
+        <div className="rounded-full w-10 h-10 overflow-hidden">
+          <img
+            className="h-auto aspect-square object-cover"
+            src={`${name}.svg`}
+            alt={`${name} 사진`}
+          />
+        </div>
       </div>
     </div>
   );
