deb5fa1a33936297609d2b03f403204505cfe6b5
서예지 <syg9272@naver.com>
2023-05-15 15:51:59 +0900

    🎨 add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Letter from "./pages/Letter";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/letter" element={<Letter />} />
+      </Routes>
     </div>
   );
 }
